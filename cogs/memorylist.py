5772173427222c9d800bfa0f614d282aa9e69fbf
Ekwok <zentetsukenshi@gmail.com>
2024-08-18 11:16:46 +0800

    now testing bot with cogs

--- cogs/memorylist.py
+++ cogs/memorylist.py
@@ -8,70 +8,9 @@ import os
 from discord.ext import commands
 from discord.ext.commands import BucketType, cog, BadArgument, command, cooldown
 from embedconfig import EmbedClass
-# from pagination import BaseView
+from pagination import PaginationView
 
 from discord.ui.select import BaseSelect
-
-class Button(discord.ui.Button['MemoryList']):
-    def __init__(self):
-        super().__init__(style=discord.ButtonStyle.secondary, label="Test")
-
-    # This function is called whenever this particular button is pressed
-    # This is part of the "meat" of the game logic
-    async def callback(self, interaction: discord.Interaction):
-        assert self.view is not None
-        view: BaseView = self.view
-
-        content = "Test"
-
-        await interaction.response.edit_message(content=content, view=view)
-
-class BaseView(discord.ui.View):
-    # interaction: discord.Interaction | None = None
-    # message: discord.Message | None = None
-
-    def __init__(self):
-        super().__init__()
-        self.add_item(Button())
-        # super().__init__(timeout=timeout)
-        # self.user = user
-
-    # async def interaction_check(self, interaction: discord.Interaction) -> bool:
-    #     if interaction.user.id != self.user.id:
-    #         await interaction.response.send_message(
-    #             "You cannot interact with this view.", ephemeral=True
-    #         )
-    #         return False
-    #     self.interaction = interaction
-    #     return True
-
-
-    # def _disable_all(self) -> None:
-    #     for item in self.children:
-    #         if isinstance(item, discord.ui.Button) or isinstance(item, BaseSelect):
-    #             item.disabled = True
-
-
-    # async def _edit(self, **kwargs: typing.Any) -> None:
-    #     if self.interaction is None and self.message is not None:
-    #         await self.message.edit(**kwargs)
-    #     elif self.interaction is not None:
-    #         try:
-    #             await self.interaction.response.edit_message(**kwargs)
-    #         except discord.InteractionResponded:
-    #             await self.interaction.edit_original_response(**kwargs)
-
-    # async def on_error(self, interaction: discord.Interaction, error: Exception, item: discord.ui.Item[BaseView]) -> None:
-    #     tb = "".join(traceback.format_exception(type(error), error, error.__traceback__))
-    #     message = f"An error occurred while processing the interaction for {str(item)}:\n```py\n{tb}\n```"
-    #     self._disable_all()
-    #     await self._edit(content=message, view=self)
-    #     self.stop()
-
-    # async def on_timeout(self) -> None:
-    #     self._disable_all()
-    #     await self._edit(view=self)
-
 
 class MemoryList(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -85,11 +24,11 @@ class MemoryList(commands.Cog):
     @commands.command()
     async def memorylist(self, ctx: commands.Context) -> None:
         # print(dir(ctx))
-        testview = BaseView()
-        await ctx.send("Testing", view=testview)
+        testview = PaginationView()
+        # await ctx.send("Testing", view=testview)
         # print(dir(view))
-        # testview.add_item(discord.ui.Button(label="Test", style=discord.ButtonStyle.blurple))
-        # testview.message = await ctx.send("Testing", view=testview)
+        testview.add_item(discord.ui.Button(label="Test", style=discord.ButtonStyle.blurple))
+        testview.message = await ctx.send("Testing", view=testview)
         # print(testview.message)
         
 
--- pagination.py
+++ pagination.py
@@ -1,77 +1,64 @@
-# Our objectives:
-# - Create a view that handles errors
-# - Create a view that disables all components after timeout
-# - Make sure that the view only processes interactions from the user who invoked the command
-
 from __future__ import annotations
 
+import logging
+import discord
 import typing
 import traceback
 
-import discord
-from discord.ui.select import BaseSelect
+class Button(discord.ui.Button):
+    def __init__(self):
+        super().__init__(style=discord.ButtonStyle.secondary, label="Test")
 
+    # This function is called whenever this particular button is pressed
+    # This is part of the "meat" of the game logic
+    async def callback(self, interaction: discord.Interaction):
+        assert self.view is not None
+        view: PaginationView = self.view
 
-class BaseView(discord.ui.View):
-    interaction: discord.Interaction | None = None
+        content = "Test"
+
+        await interaction.response.edit_message(content=content, view=view)
+
+class PaginationView(discord.ui.View):
     message: discord.Message | None = None
+    count = 0
 
-    def __init__(self, user: discord.User | discord.Member, timeout: float = 60.0):
+    def __init__(self, user: discord.User | discord.Member, timeout: float = 60.0) -> None:
         super().__init__(timeout=timeout)
-        # We set the user who invoked the command as the user who can interact with the view
         self.user = user
 
-    # make sure that the view only processes interactions from the user who invoked the command
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        # if interaction.user.id != self.user.id:
-        #     await interaction.response.send_message(
-        #         "You cannot interact with this view.", ephemeral=True
-        #     )
-        #     return False
-        print(dir(interaction))
-        await interaction.response.send_message("Hi", ephemeral=True)
-        # update the interaction attribute when a valid interaction is received
-        self.interaction = interaction
-        
-        return True
+    # checks for the view's interactions
+    async def interaction_check(self, interaction: discord.Interaction[discord.Client]) -> bool:
+        # this method should return True if all checks pass, else False is returned
+        # for example, you can check if the interaction was created by the user who
+        # ran the command:
+        if interaction.user == self.user:
+            return True
+        # else send a message and return False
+        await interaction.response.send_message(f"The command was initiated by {self.user.mention}", ephemeral=True)
+        return False
 
-    # to handle errors we first notify the user that an error has occurred and then disable all components
+    # do stuff on timeout
+    async def on_timeout(self) -> None:
+        # this method is called when the period mentioned in timeout kwarg passes.
+        # we can do tasks like disabling buttons here.
+        for button in self.children:
+            button.disabled = True  # type: ignore
+        # and update the message with the update View.
+        if self.message:
+            await self.message.edit(view=self)
 
-    def _disable_all(self) -> None:
-        # disable all components
-        # so components that can be disabled are buttons and select menus
-        for item in self.children:
-            if isinstance(item, discord.ui.Button) or isinstance(item, BaseSelect):
-                item.disabled = True
+    # adding a component using it's decorator
+    @discord.ui.button(label="0", style=discord.ButtonStyle.green, )
+    async def counter(self, inter: discord.Interaction, button: discord.ui.Button[PaginationView]) -> None:
+        self.count+=1
+        button.label = str(self.count)
+        await inter.response.edit_message(view=self)
 
-    # after disabling all components we need to edit the message with the new view
-    # now when editing the message there are two scenarios:
-    # 1. the view was never interacted with i.e in case of plain timeout here message attribute will come in handy
-    # 2. the view was interacted with and the interaction was processed and we have the latest interaction stored in the interaction attribute
-    async def _edit(self, **kwargs: typing.Any) -> None:
-        if self.interaction is None and self.message is not None:
-            # if the view was never interacted with and the message attribute is not None, edit the message
-            await self.message.edit(**kwargs)
-        elif self.interaction is not None:
-            try:
-                # if not already responded to, respond to the interaction
-                await self.interaction.response.edit_message(**kwargs)
-            except discord.InteractionResponded:
-                # if already responded to, edit the response
-                await self.interaction.edit_original_response(**kwargs)
-
-    async def on_error(self, interaction: discord.Interaction, error: Exception, item: discord.ui.Item[BaseView]) -> None:
+    # error handler for the view
+    async def on_error(
+        self, interaction: discord.Interaction[discord.Client], error: Exception, item: discord.ui.Item[typing.Any]
+    ) -> None:
         tb = "".join(traceback.format_exception(type(error), error, error.__traceback__))
         message = f"An error occurred while processing the interaction for {str(item)}:\n```py\n{tb}\n```"
-        # disable all components
-        self._disable_all()
-        # edit the message with the error message
-        await self._edit(content=message, view=self)
-        # stop the view
-        self.stop()
-
-    async def on_timeout(self) -> None:
-        # disable all components
-        self._disable_all()
-        # edit the message with the new view
-        await self._edit(view=self)
+        await interaction.response.send_message(message)
